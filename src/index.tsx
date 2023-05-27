11f419d142a1e23d8ecba905c1102a09ccd16bdd
dryjorse <kozoevabdulazim@gmail.com>
2023-05-27 17:17:27 +0300

    main-last

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
@@ -9,9 +9,10 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  // @ts-ignore
+  <HashRouter hashType="hashbang">
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
